6fbf84c7fcd71afa85e1b59a40ea63d274452c89
elias <emedim.araneda@gmail.com>
2024-11-21 14:40:20 +0100

    add files

---
+++ app/components/CarOptions.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function CarOptions() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,21 @@
-import Header from "./components/Header"
+import SearchForm from "./components/SearchForm";
+import CarOptions from "./components/CarOptions";
+import OptionsBuy from "./components/OptionsBuy";
 
 export default function Home() {
   return (
-    <Header /> 
+   <>
+    <div className="grid grid-cols-1 md:grid-cols-3 p-4 gap-4 h-screen">
+      <div className="h-full border border-gray-200 rounded-md p-5">
+        <SearchForm />
+        <h2 className="font-medium mt-4 mb-2 text-[14px]">Options</h2>
+        <OptionsBuy />
+        <h2 className="font-medium mt-4 mb-2 text-[14px]">Buy</h2>
+        <CarOptions />
+        <h2 className="font-medium mt-4 mb-2 text-[14px]">Cars</h2>
+      </div>
+    </div>
+
+   </>
   );
 }
